1ca11434328b40f411c1bf1b398a29fded7f00f6
ramasantayana <rama.santayana@uconn.edu>
2023-04-13 06:53:32 -0400

    Created function to write README

--- index.js
+++ index.js
@@ -54,8 +54,10 @@ const questions = [
       }, 
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// TODO: Create a function to write README file (DONE)
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
 function init() {
