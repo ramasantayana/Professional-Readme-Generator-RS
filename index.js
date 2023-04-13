1d5066b8afa60613387ced18a358c5f8ff7cc51e
ramasantayana <rama.santayana@uconn.edu>
2023-04-13 06:34:31 -0400

    Questions are being developed

--- index.js
+++ index.js
@@ -5,7 +5,28 @@ const path = require('path');
 const genReadme = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'title',
+      },
+      {
+        type: 'input',
+        message: 'What is the project description?',
+        name: 'description', 
+      }, 
+      {
+        type: 'installation-instructions',
+        message: 'give installation instruction for the project',
+        name: 'installation',
+      }, 
+      {
+        type: 'usage-information',
+        message: 'give usage information for the project',
+        name: 'usage',
+      }, 
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
